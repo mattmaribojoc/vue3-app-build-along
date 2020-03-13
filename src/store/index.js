70d34872e0e20a4736bb8d337da6fcefc5b7d416
Matthew Maribojoc <matthewmaribojoc@gmail.com>
2020-03-13 13:02:42 -0400

    added store methods

--- src/store/index.js
+++ src/store/index.js
@@ -23,5 +23,45 @@ class PlannerStore extends Store {
         scheduledEvents: defaultData.events
     }
   }
+
+  isTimeAvailable (id, startTime, endTime) {
+    let eventsInDay = this.state.scheduledEvents.filter(e => {
+      return (e.id !== id) && moment(e.startTime).isSame(startTime, 'day')
+    })
+
+    let range = moment.range(startTime, endTime)
+
+    for (var i = 0; i < eventsInDay.length; i++) {
+      let eRange = moment.range(eventsInDay[i].startTime, eventsInDay[i].endTime)
+
+      if (eRange.overlaps(range, { adjacent: false })) {
+        return {
+          overlap: eventsInDay[i],
+          available: false
+        }
+      }
+    }
+
+    return {
+      available: true
+    }
+  }
+
+  editEvent (newEvent) {
+    let e = this.state.scheduledEvents.find(e => e.id === newEvent.id)
+    if (!e) {
+      return false
+    }
+
+    if (this.isTimeAvailable(newEvent.id, newEvent.startTime, newEvent.endTime).available) {
+      e.name = newEvent.name
+      e.calendar = newEvent.calendar
+      e.startTime = newEvent.startTime
+      e.endTime = newEvent.endTime
+      return true
+    }
+    return false
+
+  }
 }
 export const store = new PlannerStore();
