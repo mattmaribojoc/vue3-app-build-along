4b3df13d95ce669c32eab2e1943bf7560bc0ee82
Matthew Maribojoc <matthewmaribojoc@gmail.com>
2020-03-14 15:11:46 -0400

    added basic form

--- src/components/forms/AddEvent.vue
+++ src/components/forms/AddEvent.vue
@@ -6,25 +6,97 @@
         <span @click='close'> x </span>
       </div>
       <div class='popup-content'>
-        
+        <label> Calendar </label>
+        <div 
+          v-for='cal in state.calendars'
+          :key='cal.id'
+          class='calendar-list'
+          :style='calendarStyle(cal)'
+          @click='eventData.calendar = cal.id'
+        >
+          {{ cal.name }}
+        </div>
+        <label> Name </label>
+        <input type='text' v-model='eventData.name' />
+        <label> Date </label>
+        <label> Start Time </label>
+        <input type='time' 
+          @change='changeTime("startTime", $event)'
+          @blur='updateTimeDisplay("startTime", $event)'
+        />
+        <label> End Time </label>
+        <input type='time' 
+          @change='changeTime("endTime", $event)'
+          @blur='updateTimeDisplay("endTime", $event)'
+        />
       </div>
       <div class='popup-footer'>
-        
+        <div class='popup-footer__cancel' @click='close'> Cancel </div>
+        <div class='popup-footer__confirm' @click='submit'> Confirm </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from 'vue'
 import moment from 'moment'
 import { usePopupLogic } from '../../logic/popup-logic'
+import { store } from '../../store'
+
 export default {
   setup (props, context) {
 
     const { close } = usePopupLogic('addEvent', context.emit)
 
+    const eventData = reactive({
+      calendar: '01',
+      name: '',
+      date: moment(),
+      startTime: moment(),
+      endTime: moment()
+    })
+
+    const calendarStyle = (cal) => {
+      if (eventData.calendar === cal.id) {
+        return {
+          'background-color': cal.color,
+          'color': 'white',
+          'font-weight': 'bold'
+        }
+      }
+    }
+
+    const changeTime = (property, evt) => {
+      let hour = parseInt(evt.target.value.split(':')[0])
+      let minutes = parseInt(evt.target.value.split(':')[1])
+
+      eventData[property] = eventData[property].clone().hour(hour).minute(minutes)
+
+    }
+
+    const updateTimeDisplay = (property, evt) => {
+      evt.target.value = eventData[property].format('HH:mm')
+    }
+
+    const submit = () => {
+      // verify form
+
+      // submit form 
+
+      // if success, close modal 
+
+      // otherwise, print erros
+    }
+
     return {
-      close
+      calendarStyle,
+      changeTime,
+      close,
+      eventData,
+      state: store.getState(),
+      submit,
+      updateTimeDisplay
     }
   }
 }
